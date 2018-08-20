5f7d144e207196e8a1b861464fb431a009edf0aa
care0717 <dev.care0717@gmail.com>
2018-08-20 18:09:22 +0900

    feat: 答えの表示まで完成

--- server/static/script.js
+++ server/static/script.js
@@ -12,30 +12,48 @@ function getAnswers(answer, dices, onSuccess, onError)
     }
   );
 }
+
+function printSolution(solution_example){
+  let solution = solution_example.slice(1,18).split(",").reverse()
+  let pair = [[" (", " )"], [" [", " ]"], [" {", " }"]]
+  let pair_index = 0
+  let index = 0
+  while (solution.length > 1) {
+    if (solution[index].length == 1 && solution[index].match(/[+-/*]/) != null) {
+      let exp = pair[pair_index][0] + " " + solution[index-1] + " " + solution[index] + " " + solution[index-2] + pair[pair_index][1] 
+      solution[index] = exp
+      solution.splice(index-2, 2)
+      index -= 2
+      pair_index = (pair_index + 1) % 3
+      console.log(solution)
+    }
+
+    index += 1
+  }
+  let size = solution[0].length 
+  return solution[0].slice(2,size-1)
+}
+
 function search(){
   let dices = []
   $('input[name^=dice]').each(function(){
     dices.push($(this).val()) 
   });
+  dices.sort()
   let answer = $('input[name=answer]').val()
   getAnswers(answer, dices, function(answers){
-    var table = $("table#form");
-    answers.forEach(function(answer){
-      var title = $("<td />").text(answer.answer);
-      var dice = new Array(5)
-      dice[0] = $("<td />").text(answer.dice1);
-      dice[1] = $("<td />").text(answer.dice2);
-      dice[2] = $("<td />").text(answer.dice3);
-      dice[3] = $("<td />").text(answer.dice4);
-      dice[4] = $("<td />").text(answer.dice5);
-      var tr = $("<tr />").append(title)
-      dice.forEach( d => {
-        tr = tr.append(d)
-      })
-      table.append(tr);
-    });
+    if (answers.length <= 0) {
+      $("#dummy").html('<strong>解けません</strong>')
+    } else {
+      answers.forEach(function(answer){
+        $("#dummy").html('<p id="result"><strong>答えが存在します'+ "(解の数: " + answer.solution_number + ')</strong><p>')
+        let result = "答え: " + printSolution(answer.solution_example)
+        $("#result").append('<p><a class="btn tooltipped" data-position="bottom" data-tooltip="'+ result +'">答えを表示しますか？</a></p>')
+        $('.tooltipped').tooltip();
+      });
+    }
   });
 }
 $(function(){
-  $(".button").click(search);
+  $("#button").click(search);
 })
